3fbf6ddcac86fceb941f71959a30052516cf310d
Henrique Oliveira <riikkii@gmail.com>
2017-10-16 16:26:25 -0200

    first commit

---
+++ app/src/main/java/agency/tango/materialintro/CustomSlide.java
@@ -0,0 +1,54 @@
+package agency.tango.materialintro;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import agency.tango.materialintroscreen.SlideFragment;
+
+public class CustomSlide extends SlideFragment {
+
+    private TextView wtTitle, wtDescription;
+    private ImageView wtImage;
+
+    private String title, description, image;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_custom_slide, container, false);
+
+        wtTitle = (TextView) view.findViewById(R.id.wtTitle);
+        wtDescription = (TextView) view.findViewById(R.id.wtDescription);
+        wtImage = (ImageView) view.findViewById(R.id.wtImage);
+
+
+        return view;
+    }
+
+    @Override
+    public int backgroundColor() {
+        return R.color.custom_slide_background;
+    }
+
+    @Override
+    public int buttonsColor() {
+        return R.color.custom_slide_buttons;
+    }
+
+    @Override
+    public boolean canMoveFurther() {
+        return true;
+    }
+
+    @Override
+    public String cantMoveFurtherErrorMessage() {
+        return getString(R.string.error_message);
+    }
+}
